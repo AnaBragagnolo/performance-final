728697dd4bd2ea155103d3d9d7ea71a1175bf3c0
AnaLigia <analigiab@gmail.com>
2025-12-19 19:48:39 -0300

    import para report html

--- test/k6/login.test.js
+++ test/k6/login.test.js
@@ -1,14 +1,18 @@
 import { SharedArray } from 'k6/data';
 import { group } from 'k6';
 import { login } from './helpers/auth.js';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
 
 const loginData = new SharedArray('login data', function () {
   return JSON.parse(open('./data/login.test.data.json'));
 });
 
 export const options = {
-  vus: 3,
-  iterations: 3,
+  stages: [
+  { duration: '20s', target: 3 },
+  { duration: '20s', target: 3 },
+  { duration: '10s', target: 0 }
+],
   thresholds: {
     http_req_duration: ['p(95) < 2000']
   }
@@ -21,3 +25,12 @@ export default function () {
     login(data.email, data.password);
   });
 }
+
+export function handleSummary(data) {
+  return {
+    'reports/login-report.html': htmlReport(data),
+    'reports/login-summary.json': JSON.stringify(data)
+  };
+}
+
+
